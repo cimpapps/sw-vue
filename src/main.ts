52e202f8ddafc7867a93396112ce5f75130b002f
catalincimpoeru <catalincimpoeru24@gmail.com>
2020-07-20 14:48:12 +0300

    add vuetify plugin

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import GridComponent from "@/components/shared/DataTableComponent.vue";
+import vuetify from "./plugins/vuetify"
+import Vuetify from 'vuetify';
 
 Vue.config.productionTip = false;
 
@@ -10,6 +11,7 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
 
---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,10 @@
+import Vue from "vue";
+import Vuetify from "vuetify";
+import 'vuetify/dist/vuetify.min.css'
+
+
+Vue.use(Vuetify);
+
+const options = {};
+const vuetify = new Vuetify(options);
+export default vuetify;
